11ebbe304d44dc47c6d1b9f9ae675b40fa02604b
Yvan Ngassa <yvanngassa@gmail.com>
2016-10-06 21:05:07 +0300

    Nko prototype

---
+++ myScript.js
@@ -0,0 +1,42 @@
+     <script type="text/javascript">
+            function changeText1(value) {
+                document.getElementById('mainPage').innerHTML = "You pressed 1 ";
+                if(value == "Button 3")
+                {
+                    document.getElementById('pText').setAttribute('style', 'color: green');}
+                }
+        </script>
+
+        <script type="text/javascript">
+            function changeText2(value) {
+                document.getElementById('mainPage').innerHTML = "You press 2";
+                }
+        </script>
+         <script type="text/javascript">
+            function changeText3(value) {
+                document.getElementById('mainPage').innerHTML = '<div style="text-align:center"><h2>Hello world.</h2><p>bebe</p><form action="mailto:someone@example.com" method="post" enctype="text/plain">Name:<br><input type="text" name="name"><br></form></div>';
+                }
+        </script>
+         <script type="text/javascript">
+            function changeText4(value) {
+                document.getElementById('mainPage').innerHTML = "You press 4 ";
+                document.getElementById('image').innerHTML ='<img src=\'ong_header.jpg\'>';
+                }
+        </script>
+         <script type="text/javascript">
+            function changeText5(value) {
+                document.getElementById('mainPage').innerHTML = "Powerhill was incorporated in accordance with the laws of the British Virgin Islands in December 2003, and commenced operation with mid-scale limited service hotels and commercial property development and management in 2005. Limited service hotels do not contain restaurants and all amenities are provided by the staff at the front desk or housekeeping. Powerhill conducted its operations through three wholly owned subsidiaries in the PRC, namely Shanghai HanTing Hotel Management Group, Ltd., or Shanghai HanTing, HanTing Xingkong (Shanghai) Hotel Management Co., Ltd., or HanTing Xingkong, and Lishan Property (Suzhou) Co., Ltd., or Suzhou Property. In August 2006, Suzhou Property transferred its equity interests in three leased hotels to Shanghai HanTing in exchange for Shanghai HanTing's equity interest in Shanghai Shuyu Co., Ltd., which was primarily engaged in the business of sub-leasing and managing real estate properties in technology parks.  ... More ...  Read more: http://www.nasdaq.com/symbol/htht#ixzz4LwbPFDWb";
+
+
+                document.getElementById('images').innerHTML ='<img id="images" src=\'ong_header.jpg\'>';
+                
+                }
+        </script>
+
+         <script type="text/javascript">
+            
+            function changeText6(value) {
+                
+                document.getElementById('images').innerHTML = '<img id="images" src=\'ong11.jpg\'>';
+                }
+        </script>
